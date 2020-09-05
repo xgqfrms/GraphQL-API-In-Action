07cacfc4f621c0268b737c373ccbe2770dc29ad0
xgqfrms <xgqfrms@outlook.com>
2020-09-06 00:00:59 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# GraphQL-API-In-Action
-GraphQL API In Action
+# GraphQL API In Action
+
+> GraphQL API
+
+```sh
+$ yarn add express express-graphql graphql
+# OR 
+$ npm i -S express express-graphql graphql
+
+```
+
+https://www.cnblogs.com/xgqfrms/p/13620147.html
+
+
+## jest
+
+
+```sh
+# jest
+$ yarn add -D jest
+
+# babel
+$ yarn add -D babel-jest @babel/core @babel/preset-env babel-plugin-dynamic-import-node
+
+```
---
+++ src/index.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/**
+ *
+ * @author xgqfrms
+ * @license MIT
+ * @copyright xgqfrms
+ * @created 2020-09-01
+ * @modified
+ *
+ * @description GraphQL API In Action
+ * @difficulty Easy Medium Hard
+ * @complexity O(n)
+ * @augments
+ * @example
+ * @link
+ * @solutions
+ *
+ */
+
+const log = console.log;
+
+const GraphQLAPI = (datas = [], debug = false) => {
+  let result = ``;
+  // do something...
+  return result;
+};
+
+
+
+export default GraphQLAPI;
+
+export {
+  GraphQLAPI,
+};
